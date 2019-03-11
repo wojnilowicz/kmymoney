31e2e2000e92549a4525c828b7ec795c1fe76463
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-03-11 19:16:17 +0100

    Build less dependencies

--- packaging/linux/appimage/build-deps.sh
+++ packaging/linux/appimage/build-deps.sh
@@ -66,10 +66,10 @@ cmake --build . --target ext_png
 # cmake --build . --target ext_jpeg #this causes build failures in Qt 5.10
 # cmake --build . --target ext_qt
 cmake --build . --target ext_boost
-cmake --build . --target ext_kcmutils
-cmake --build . --target ext_kactivities
-cmake --build . --target ext_kitemmodels
-cmake --build . --target ext_kitemviews
+# cmake --build . --target ext_kcmutils
+# cmake --build . --target ext_kactivities
+# cmake --build . --target ext_kitemmodels
+# cmake --build . --target ext_kitemviews
 # cmake --build . --target ext_kholidays
 # cmake --build . --target ext_kidentitymanagement
 # cmake --build . --target ext_kcontacts
