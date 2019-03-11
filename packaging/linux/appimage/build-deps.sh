1f0d0d41087ac4d1222e23a81746ff13c2483303
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-03-11 18:15:05 +0100

    Try to cache some results

--- packaging/linux/appimage/build-deps.sh
+++ packaging/linux/appimage/build-deps.sh
@@ -58,18 +58,18 @@ cmake $KMYMONEY_SOURCES/3rdparty -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX -DE
 
 # Now start building everything we need, in the appropriate order
 cmake --build . --target ext_iconv
-cmake --build . --target ext_lzma
-cmake --build . --target ext_xml
-cmake --build . --target ext_gettext
-cmake --build . --target ext_xslt
-cmake --build . --target ext_png
+# cmake --build . --target ext_lzma
+# cmake --build . --target ext_xml
+# cmake --build . --target ext_gettext
+# cmake --build . --target ext_xslt
+# cmake --build . --target ext_png
 # cmake --build . --target ext_jpeg #this causes build failures in Qt 5.10
 # cmake --build . --target ext_qt
-cmake --build . --target ext_boost
-cmake --build . --target ext_kcmutils
-cmake --build . --target ext_kactivities
-cmake --build . --target ext_kitemmodels
-cmake --build . --target ext_kitemviews
+# cmake --build . --target ext_boost
+# cmake --build . --target ext_kcmutils
+# cmake --build . --target ext_kactivities
+# cmake --build . --target ext_kitemmodels
+# cmake --build . --target ext_kitemviews
 # cmake --build . --target ext_kholidays
 # cmake --build . --target ext_kidentitymanagement
 # cmake --build . --target ext_kcontacts
