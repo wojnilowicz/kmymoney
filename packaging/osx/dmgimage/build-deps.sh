876f179e8e0a7a2608ecee952bcb8a74016bab10
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-03-13 20:39:37 +0100

    Structure runtime timeout better

---
+++ packaging/osx/dmgimage/build-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Halt on errors and be verbose about what we are doing
+set -e
+set -x
+
+brew install kde-mac/kde/kf5-kcmutils
+brew install kde-mac/kde/kf5-kactivities
+brew install kde-mac/kde/kf5-kitemmodels
+brew install kde-mac/kde/kf5-kitemviews
