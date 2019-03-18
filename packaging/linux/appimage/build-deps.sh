ec4d16fed12df56f66f94dd582a3c4c5dbabfed0
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-03-18 18:39:23 +0100

    Try to build dependencies on Linux

--- packaging/linux/appimage/build-deps.sh
+++ packaging/linux/appimage/build-deps.sh
@@ -13,23 +13,24 @@ cd $CMAKE_BUILD_PREFIX
 cmake -GNinja \
       $KMYMONEY_SOURCES/3rdparty \
       -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX \
+      -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DEXT_DOWNLOAD_DIR=$DOWNLOADS_DIR
 
 # Now start building everything we need, in the appropriate order
-# cmake --build . --target ext_iconv -- -j${CPU_COUNT}
-# cmake --build . --target ext_lzma -- -j${CPU_COUNT}
-# cmake --build . --target ext_xml -- -j${CPU_COUNT}
-# cmake --build . --target ext_gettext -- -j${CPU_COUNT}
-# cmake --build . --target ext_xslt -- -j${CPU_COUNT}
-# cmake --build . --target ext_png -- -j${CPU_COUNT}
+cmake --build . --target ext_iconv -- -j${CPU_COUNT}
+cmake --build . --target ext_lzma -- -j${CPU_COUNT}
+cmake --build . --target ext_xml -- -j${CPU_COUNT}
+cmake --build . --target ext_gettext -- -j${CPU_COUNT}
+cmake --build . --target ext_xslt -- -j${CPU_COUNT}
+cmake --build . --target ext_png -- -j${CPU_COUNT}
 # cmake --build . --target ext_jpeg -- -j${CPU_COUNT} #this causes build failures in Qt 5.10
 # cmake --build . --target ext_qt -- -j${CPU_COUNT}
-# cmake --build . --target ext_boost -- -j${CPU_COUNT}
-# cmake --build . --target ext_kcmutils -- -j${CPU_COUNT}
-# cmake --build . --target ext_kactivities -- -j${CPU_COUNT}
-# cmake --build . --target ext_kitemmodels -- -j${CPU_COUNT}
-# cmake --build . --target ext_kitemviews -- -j${CPU_COUNT}
+cmake --build . --target ext_boost -- -j${CPU_COUNT}
+cmake --build . --target ext_kcmutils -- -j${CPU_COUNT}
+cmake --build . --target ext_kactivities -- -j${CPU_COUNT}
+cmake --build . --target ext_kitemmodels -- -j${CPU_COUNT}
+cmake --build . --target ext_kitemviews -- -j${CPU_COUNT}
 # cmake --build . --target ext_kholidays -- -j${CPU_COUNT}
 # cmake --build . --target ext_kidentitymanagement -- -j${CPU_COUNT}
 # cmake --build . --target ext_kcontacts -- -j${CPU_COUNT}
