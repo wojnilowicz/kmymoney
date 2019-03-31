a58c1491790a472136d94910f7d0fdf7171396db
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-03-31 12:50:59 +0200

    Try to fix build

--- packaging/osx/dmgimage/build-image.sh
+++ packaging/osx/dmgimage/build-image.sh
@@ -197,7 +197,7 @@ createDMG () {
     sync
 
     hdiutil detach $device
-    hdiutil convert "kmymoney.temp.dmg" -format ULFO -imagekey -o kmymoney-out.dmg
+    hdiutil convert -format ULFO -o kmymoney-out.dmg -imagekey kmymoney.temp.dmg
 
     # Add git version number
 #     GIT_SHA=$(grep "#define KMYMONEY_GIT_SHA1_STRING" ${KIS_BUILD_DIR}/libs/version/kmymoneygitversion.h | awk '{gsub(/"/, "", $3); printf $3}')
