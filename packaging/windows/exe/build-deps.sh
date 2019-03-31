7ecfe0304f22c198219de1ecfef90e46b3449af3
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-03-31 17:58:40 +0200

    Introduce windows build

---
+++ packaging/windows/exe/build-deps.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Build all KMyMoney's dependencies on Microsoft Windows.
+#
+#
+set -eux
+
+# Switch directory in order to put all build files in the right place
+cd $CMAKE_BUILD_PREFIX
+
+# cd $CMAKE_BUILD_PREFIX
+# NINJA_EXECUTABLE=$DEPS_INSTALL_PREFIX/bin/ninja
+# if [ ! -f $NINJA_EXECUTABLE ] ||
+#    [ $(tr . 0 <<< $($NINJA_EXECUTABLE --version)) -lt $(tr . 0 <<< "1.9.0") ] ; then
+#   rm -fr ninja
+#   git clone --single-branch -b release --depth 1 git://github.com/ninja-build/ninja.git
+#   cd ninja
+#   python3 configure.py --bootstrap
+#   mkdir -p $DEPS_INSTALL_PREFIX/bin
+#   install -vm755 ninja $DEPS_INSTALL_PREFIX/bin
+#   cd ..
+#   rm -fr ninja
+# fi
+
+# Configure the dependencies for building
+cmake  \
+      $KMYMONEY_SOURCES/3rdparty \
+      -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX \
+      -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DEXT_DOWNLOAD_DIR=$DOWNLOADS_DIR
+
+# bash -c "for i in {1..5};do sleep 9m; echo \"Still building\"; done;" &
+# Now start building everything we need, in the appropriate order
+# cmake --build . --target ext_iconv -- -j${CPU_COUNT}
+# cmake --build . --target ext_xml -- -j${CPU_COUNT}
+# cmake --build . --target ext_gettext -- -j${CPU_COUNT}
+# cmake --build . --target ext_xslt -- -j${CPU_COUNT}
+cmake --build . --target ext_png -- -j${CPU_COUNT}
+# cmake --build . --target ext_jpeg -- -j${CPU_COUNT} #this causes build failures in Qt 5.10
+# cmake --build . --target ext_qt -- -j${CPU_COUNT}
+# cmake --build . --target ext_qtbase -- -j${CPU_COUNT}
+# cmake --build . --target ext_qttools -- -j${CPU_COUNT}
+# cmake --build . --target ext_qtdeclarative -- -j${CPU_COUNT}
+# cmake --build . --target ext_qtx11extras -- -j${CPU_COUNT}
+# cmake --build . --target ext_qtwebchannel -- -j${CPU_COUNT}
+# cmake --build . --target ext_qtwebengine -- -j${CPU_COUNT}
+# cmake --build . --target ext_boost -- -j${CPU_COUNT}
+# cmake --build . --target ext_kcmutils -- -j${CPU_COUNT}
+# cmake --build . --target ext_kactivities -- -j${CPU_COUNT}
+# cmake --build . --target ext_kitemmodels -- -j${CPU_COUNT}
+# cmake --build . --target ext_kitemviews -- -j${CPU_COUNT}
+# cmake --build . --target ext_kholidays -- -j${CPU_COUNT}
+# cmake --build . --target ext_kidentitymanagement -- -j${CPU_COUNT}
+# cmake --build . --target ext_kcontacts -- -j${CPU_COUNT}
+# cmake --build . --target ext_akonadi -- -j${CPU_COUNT}
+# cmake --build . --target ext_alkimia -- -j${CPU_COUNT}
+# cmake --build . --target ext_kdiagram -- -j${CPU_COUNT}
+# cmake --build . --target ext_aqbanking -- -j${CPU_COUNT}
+# cmake --build . --target ext_gpgme -- -j${CPU_COUNT}
+# cmake --build . --target ext_sqlcipher -- -j${CPU_COUNT}
+# cmake --build . --target ext_ofx -- -j${CPU_COUNT}
+# cmake --build . --target ext_ical -- -j${CPU_COUNT}
---
+++ packaging/windows/exe/build.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# Wrapper for all build scripts, which sets many shared variables.
+
+# Halt on errors and be verbose about what we are doing
+set -eux
+
+# Read in our parameters
+BUILD_TYPE=$1
+export WORKSPACE_PATH=$2
+export KMYMONEY_SOURCES=$3
+
+# We want to use $prefix/deps/usr/ for all our dependencies
+if [ $BUILD_TYPE == "deps" ] || [ $BUILD_TYPE == "kmymoney" ] || [ $BUILD_TYPE == "image" ];then
+  export CMAKE_BUILD_PREFIX=$WORKSPACE_PATH/$BUILD_TYPE-build
+else
+  echo "ERROR: no valid build type."
+  exit 1
+fi
+
+export DEPS_INSTALL_PREFIX=$WORKSPACE_PATH/deps-install
+export KMYMONEY_INSTALL_PREFIX=$WORKSPACE_PATH/kmymoney-install/usr
+export IMAGE_INSTALL_PREFIX=$WORKSPACE_PATH/image-install
+export DOWNLOADS_DIR=$WORKSPACE_PATH/downloads
+
+# A kmymoney build layout looks like this:
+# kmymoney/ -- the source directory
+# kmymoney-build/ -- build directory for kmymoney itself
+# kmymoney-install/ -- install directory for kmymoney and the dependencies
+# deps-build/ -- build directory for the dependencies
+# deps-install/ -- the location for the built dependencies
+
+# Make sure our build directory exists
+mkdir -p $CMAKE_BUILD_PREFIX
+mkdir -p $DEPS_INSTALL_PREFIX
+mkdir -p $KMYMONEY_INSTALL_PREFIX
+mkdir -p $IMAGE_INSTALL_PREFIX
+mkdir -p $DOWNLOADS_DIR
+
+# qjsonparser, used to add metadata to the plugins needs to work in a en_US.UTF-8 environment.
+# That's not always the case, so make sure it is
+export LC_ALL=en_US.UTF-8
+export LANG=en_us.UTF-8
+
+# Setup variables needed to help everything find what we build
+export LD_LIBRARY_PATH=\
+$DEPS_INSTALL_PREFIX/lib:\
+$DEPS_INSTALL_PREFIX/lib/x86_64-linux-gnu:\
+$DEPS_INSTALL_PREFIX/openssl/lib:\
+${LD_LIBRARY_PATH:-}
+
+export PATH=\
+$DEPS_INSTALL_PREFIX/bin:\
+$DEPS_INSTALL_PREFIX/openssl/bin:\
+${PATH:-}
+
+export PKG_CONFIG_PATH=\
+$DEPS_INSTALL_PREFIX/share/pkgconfig:\
+$DEPS_INSTALL_PREFIX/lib/pkgconfig:\
+$DEPS_INSTALL_PREFIX/openssl/lib/pkgconfig:\
+${PKG_CONFIG_PATH:-}
+
+export CMAKE_PREFIX_PATH=\
+$DEPS_INSTALL_PREFIX:\
+$DEPS_INSTALL_PREFIX/openssl:\
+${CMAKE_PREFIX_PATH:-}
+
+# Get available processors count
+export CPU_COUNT=`grep processor /proc/cpuinfo | wc -l`
+
+if [ $BUILD_TYPE == "deps" ];then
+  sh $KMYMONEY_SOURCES/packaging/windows/exe/build-deps.sh
+elif [ $BUILD_TYPE == "kmymoney" ];then
+  sh $KMYMONEY_SOURCES/packaging/windows/exe/build-kmymoney.sh
+elif [ $BUILD_TYPE == "image" ];then
+  sh $KMYMONEY_SOURCES/packaging/windows/exe/build-image.sh
+fi
