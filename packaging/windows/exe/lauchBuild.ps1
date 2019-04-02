388e73b6fff0d762bf14d55e548397b92eb76779
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 19:08:59 +0200

    Test MSYS2 environment

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,3 +1,6 @@
+bash --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
+bash -lc "pacman -S --needed --noconfirm git"
 bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
+
