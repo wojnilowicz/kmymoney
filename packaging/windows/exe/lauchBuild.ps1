35c4c64a4249c510d83387317f18bd9a93d37107
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 17:25:40 +0200

    Try to start on Windows

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,3 +1,3 @@
-$homeUnix= "/"(($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
-$buildUnix= "/"(($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-bash $buildUnix/packaging/windows/exe/build.sh deps $homeUnix/workspace $buildUnix
+$homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
+$buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
+bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
