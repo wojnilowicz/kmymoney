0bf51ec6b1612e8382b2a9db1c210159acd608bf
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 20:41:39 +0200

    Try to build on Windows

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,9 +1,8 @@
 Write-Host $Env:Path
 $Env:Path='C:\tools\msys64\usr\bin;' + $Env:Path
-C:\tools\msys64\usr\bin\bash.exe --version
+bash --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-C:\tools\msys64\usr\bin\bash.exe -lc "pacman -Ss git"
-C:\tools\msys64\usr\bin\bash.exe /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
-bash --version
+bash -lc "pacman -Ss git"
+bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
 
