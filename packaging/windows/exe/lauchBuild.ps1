691548e3da05fe4915c5146ae971a81ec37d670c
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 19:48:28 +0200

    Try to run MSYS2 bash

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,6 +1,6 @@
-bash --version
+C:\tools\msys64\usr\bin\bash.exe --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-bash -lc "pacman -S --needed --noconfirm git"
-bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
+C:\tools\msys64\usr\bin\bash.exe -lc "pacman -S --needed --noconfirm git"
+C:\tools\msys64\usr\bin\bash.exe /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
 
