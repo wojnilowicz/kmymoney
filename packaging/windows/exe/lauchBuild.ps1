a1673223a2c7f3d96940ced385cc0de97811768c
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 21:11:03 +0200

    Try to get rid of git bash from PATH

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,5 +1,5 @@
 Write-Host $Env:Path
-$Env:Path='C:\tools\msys64\usr\bin;' + $Env:Path
+$Env:Path='C:\tools\msys64\usr\bin;C:\Program Files\CMake\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32\OpenSSH;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin'
 bash --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
