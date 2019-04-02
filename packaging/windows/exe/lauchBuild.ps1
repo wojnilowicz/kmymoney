0846c87f4054e0fa35449fd514a4f6e3e7e9e4d5
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 20:29:26 +0200

    See if MSYS is callable

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,6 +1,9 @@
+Write-Host $Env:Path
+$Env:Path='C:\tools\msys64\usr\bin;' + $Env:Path
 C:\tools\msys64\usr\bin\bash.exe --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-C:\tools\msys64\usr\bin\bash.exe -lc "pacman -S --needed --noconfirm git"
+C:\tools\msys64\usr\bin\bash.exe -lc "pacman -Ss git"
 C:\tools\msys64\usr\bin\bash.exe /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
+bash --version
 
