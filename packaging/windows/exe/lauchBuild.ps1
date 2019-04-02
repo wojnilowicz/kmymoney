b65ebffc0ff6d08ef9e1b1407282ca5d182d195b
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-02 17:03:45 +0200

    Correct ps script

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,1 +1,1 @@
-bash $Env:TRAVIS_BUILD_DIR/packaging/windows/exe/build.sh deps $$Env:HOME/workspace $$Env:TRAVIS_BUILD_DIR
+bash $Env:TRAVIS_BUILD_DIR/packaging/windows/exe/build.sh deps $Env:HOME/workspace $Env:TRAVIS_BUILD_DIR
