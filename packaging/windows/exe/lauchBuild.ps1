d96d0c0ba13f5ef11296a9fa5c429ca96c1f4dff
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-03 18:54:31 +0200

    Install dependencies only

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,11 +1,8 @@
 Write-Host $Env:Path
-# $Env:Path='C:\tools\msys64\usr\bin;C:\Program Files\CMake\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32\OpenSSH;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin'
-
 $Env:Path='C:\tools\msys64\usr\bin;' + $Env:Path
-
 bash --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
 bash -lc "pacman -S --needed --noconfirm make"
-bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
+# bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
 
