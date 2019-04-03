b6f2f52ee509fb5da5d1346d6b6fa0de2291a4be
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-03 18:36:15 +0200

    Do unattended pacman install

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -6,6 +6,6 @@ $Env:Path='C:\tools\msys64\usr\bin;' + $Env:Path
 bash --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-bash -lc "pacman -S make"
+bash -lc "pacman -S --needed --noconfirm make"
 bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
 
