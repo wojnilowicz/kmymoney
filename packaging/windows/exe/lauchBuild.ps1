5a3ac13fbe011523a8c944561427fdb1cf82237b
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-03 19:19:38 +0200

    Install zlib dependency

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -3,6 +3,6 @@ $Env:Path='C:\tools\msys64\usr\bin;' + $Env:Path
 bash --version
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-bash -lc "pacman -S --needed --noconfirm make"
-# bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
+bash -lc "pacman -S --needed --noconfirm make mingw-w64-x86_64-zlib"
+bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
 
