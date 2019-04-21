5cc47799e0f82f2fd3e96d215a6a5936be72d27c
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-21 11:14:33 +0200

    Try to install msys2 persistently

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -5,7 +5,7 @@ $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
 if ( $args[0]="pacman-deps" ) {
   Write-Host "In Pacman deps"
-  # bash -lc "pacman -S --needed --noconfirm make patch mingw-w64-x86_64-ninja"
+  bash -lc "pacman -S --needed --noconfirm make patch mingw-w64-x86_64-ninja"
 } else {
   bash /$buildUnix/packaging/windows/exe/build.sh $args[0] /c /$buildUnix
 }
