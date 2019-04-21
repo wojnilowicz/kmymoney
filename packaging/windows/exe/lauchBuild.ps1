163b7b4a3802e29811c9983540122a86c437e091
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-21 05:47:54 +0200

    Try to install python

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -2,7 +2,7 @@ Write-Host $Env:Path
 $Env:Path="C:\msys64\usr\bin;C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;$Env:Path"
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-bash -lc "pacman -Ss git"
+# bash -lc "pacman -Ss git"
 bash -lc "pacman -S --needed --noconfirm make patch mingw-w64-x86_64-ninja"
-# bash /$buildUnix/packaging/windows/exe/build.sh deps /$homeUnix/workspace /$buildUnix
+# bash /$buildUnix/packaging/windows/exe/build.sh $args[0] /$homeUnix/workspace /$buildUnix
 
