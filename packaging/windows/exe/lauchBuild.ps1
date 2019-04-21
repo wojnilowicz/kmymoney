f28fdaa1402669dc2bbd57cf951de34777d3a51e
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-21 11:48:47 +0200

    Correct pip3 location

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,11 +1,11 @@
 Write-Host $Env:Path
-Write-Host $args[0]
-$Env:Path="C:\Python37\Script;C:\Python37\bin;C:\msys64\usr\bin;C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;$Env:Path"
+# $Env:Path="C:\Python37\Script;C:\Python37\bin;C:\msys64\usr\bin;C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;$Env:Path"
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
 if ( $args[0]="pacman-deps" ) {
-  Write-Host "In Pacman deps"
   bash -lc "pacman -S --needed --noconfirm make patch mingw-w64-x86_64-ninja"
+} elseif ($args[0]="update-msys2") {
+  bash -lc "pacman -Syu"
 } else {
   bash /$buildUnix/packaging/windows/exe/build.sh $args[0] /c /$buildUnix
 }
