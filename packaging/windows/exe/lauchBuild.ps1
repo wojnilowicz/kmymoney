2bb493c9b8419932d947c8a6b62238834af9e89b
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-21 16:51:13 +0200

    Point to the right python path

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,5 +1,5 @@
 Write-Host $Env:Path
-$Env:Path="C:\Python37\Scripts;C:\Python37\bin;C:\msys64\usr\bin;C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;C:\Program Files\Git\cmd"
+$Env:Path="C:\Python37\Scripts;C:\Python37;C:\msys64\usr\bin;C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;C:\Program Files\Git\cmd"
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
 if ( $args[0] -eq "pacman-deps") {
