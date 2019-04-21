15ade0d160e020333a40a08afbd0d5c19b908dd3
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-04-21 10:09:03 +0200

    Try to use timeouts

--- packaging/windows/exe/lauchBuild.ps1
+++ packaging/windows/exe/lauchBuild.ps1
@@ -1,8 +1,14 @@
 Write-Host $Env:Path
+Write-Host $args[0]
 $Env:Path="C:\msys64\usr\bin;C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;$Env:Path"
 $homeUnix= (($Env:HOME -replace "\\","/") -replace ":","").ToLower().Trim("/")
 $buildUnix= (($Env:TRAVIS_BUILD_DIR -replace "\\","/") -replace ":","").ToLower().Trim("/")
-# bash -lc "pacman -Ss git"
-bash -lc "pacman -S --needed --noconfirm make patch mingw-w64-x86_64-ninja"
-# bash /$buildUnix/packaging/windows/exe/build.sh $args[0] /$homeUnix/workspace /$buildUnix
+if ( $args[0]="pacman-deps" ) {
+  Write-Host "In Pacman deps"
+  # bash -lc "pacman -S --needed --noconfirm make patch mingw-w64-x86_64-ninja"
+} else {
+  bash /$buildUnix/packaging/windows/exe/build.sh $args[0] /$homeUnix/workspace /$buildUnix
+}
+
+
 
