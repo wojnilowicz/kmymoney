5036a217d6b99f7ca6c22f36be7083805c23ed45
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-05-03 17:39:49 +0200

    Build bison for MinGW

--- packaging/windows/exe/build-deps.sh
+++ packaging/windows/exe/build-deps.sh
@@ -79,6 +79,7 @@ fi
 cmake --build . --target ext_qtwinextras -- -j${CPU_COUNT}
 # cmake --build . --target ext_qtwebengine -- -j${CPU_COUNT}
 
+cmake --build . --target ext_bison -- -j${CPU_COUNT}
 cmake --build . --target ext_gperf -- -j${CPU_COUNT} # required by KCodecs
 cmake --build . --target ext_kitemviews -- -j${CPU_COUNT}
 cmake --build . --target ext_kdewin -- -j${CPU_COUNT} # required by KWindowSystem
