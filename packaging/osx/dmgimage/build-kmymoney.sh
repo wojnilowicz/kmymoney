23c925e9230ca378c4b3f4551c71167bd01c36d8
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2019-05-25 10:09:05 +0200

    Try to build further

--- packaging/osx/dmgimage/build-kmymoney.sh
+++ packaging/osx/dmgimage/build-kmymoney.sh
@@ -12,15 +12,14 @@ cd $CMAKE_BUILD_PREFIX
 cmake -G "Unix Makefiles" \
       $KMYMONEY_SOURCES \
       -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX \
+      -DKDE_INSTALL_BUNDLEDIR=$CMAKE_INSTALL_PREFIX/Applications/KDE \
       -DCMAKE_PREFIX_PATH=$DEPS_INSTALL_PREFIX \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DBUILD_TESTING=FALSE \
       -DIS_APPIMAGE=FALSE \
       -DAPPLE_SUPPRESS_X11_WARNING=ON \
       -DCMAKE_MACOSX_RPATH=ON \
-      -DKDE_SKIP_RPATH_SETTINGS=ON \
-      -DMACOSX_BUNDLE=ON \
-      -DMACOSX_BUNDLE_BUNDLE_NAME=KMyMoneyNEXT
+      -DKDE_SKIP_RPATH_SETTINGS=ON
 
 # Build and Install KMyMoney (ready for the next phase)
 cmake --build . --target install -- -j${CPU_COUNT}
